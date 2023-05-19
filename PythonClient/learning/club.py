e9ef479662d89fd06f3879be1ba52918f7536998
MohammadMahdi <parchamimahdimohammad@gmail.com>
2023-05-19 18:47:32 +0330

    demonsclob and souls

---
+++ PythonClient/learning/club.py
@@ -0,0 +1,16 @@
+#souls fight for the becoming of champions of the demons club
+class DemonsClub:
+       
+    def __init__(self):
+        self.souls = []
+        
+             
+        
+class Soul:
+    
+    def __init__(self) -> None:
+        pass
+    
+    @staticmethod
+    def sex(soul1, soul2):
+        pass
