f2c2b4b006764713ad5de07c04bdb94e523a0e10
Mahan Veisi <mahan8292@gmail.com>
2023-06-24 15:42:48 +0330

    get direction - get action - first stages of minimax tree - a compilable decide code added, all tested. let's gooo

--- PythonClient/ai.py
+++ PythonClient/ai.py
@@ -39,8 +39,7 @@ class Soul(nn.Module):
         return x
 
 
-def game_result(new_world, action):
-    pass
+# todo: create a class for handling the game including game_result
 
 
 class AI(RealtimeAI):
@@ -48,46 +47,240 @@ class AI(RealtimeAI):
     def __init__(self, world):
         super(AI, self).__init__(world)
 
-    def initialize(self):
-        pass
+    def calculate_score(self, new_world, is_us, current_agent):
+        added_score = 0
+        opp_added_score = 0
+        agent_side = self.my_side if is_us else self.other_side
+        wall_breaker_rem_time = current_agent.wall_breaker_rem_time
+        wall_breaker_cooldown = current_agent.wall_breaker_cooldown
+
+        destination = new_world.board[current_agent.position.y][current_agent.position.x]
+
+        if destination == ECell.Empty:
+            added_score += new_world.constants.wall_score_coefficient
+        else:
+            if (is_us and self.my_side == 'Yellow' and destination == ECell.YellowWall) or \
+                    (not is_us and self.my_side == 'Yellow' and destination == ECell.BlueWall) or \
+                    (is_us and self.my_side == 'Blue' and destination == ECell.BlueWall) or \
+                    (not is_us and self.my_side == 'Blue' and destination == ECell.YellowWall):
+                if wall_breaker_cooldown == 0 and wall_breaker_rem_time > 0:
+                    added_score += 0
+                    current_agent.wall_breaker_rem_time -= 1
+                else:
+                    if current_agent.health > 1:
+                        added_score += 0
+                        current_agent.health -= 1
+                    else:
+                        added_score += new_world.constants.my_wall_crash_score
+            elif (is_us and self.my_side == 'Yellow' and destination == ECell.BlueWall) or \
+                    (not is_us and self.my_side == 'Yellow' and destination == ECell.YellowWall) or \
+                    (is_us and self.my_side == 'Blue' and destination == ECell.YellowWall) or \
+                    (not is_us and self.my_side == 'Blue' and destination == ECell.BlueWall):
+                added_score += new_world.constants.wall_score_coefficient
+                opp_added_score -= new_world.constants.wall_score_coefficient
+                if wall_breaker_cooldown == 0 and wall_breaker_rem_time > 0:
+                    new_world.agents[agent_side].wall_breaker_rem_time -= 1
+                else:
+                    if current_agent.health > 1:
+                        current_agent.health -= 1
+                    else:
+                        added_score += new_world.constants.enemy_wall_crash_score
+            elif destination == ECell.AreaWall:
+                added_score += new_world.constants.area_wall_crash_score
+
+        return added_score, opp_added_score, current_agent
+
+    def game_result(self, new_world, action, is_us):
+        # creating agent my_side
+        current_agent = copy.deepcopy(new_world.agents[self.my_side]) if is_us else \
+            copy.deepcopy(new_world.agents[self.other_side])
+
+        # direction change
+        if action == "up":
+            current_agent.position.y -= 1
+            current_agent.direction = EDirection.Up
+        elif action == "down":
+            current_agent.position.y += 1
+            current_agent.direction = EDirection.Down
+        elif action == "left":
+            current_agent.position.x -= 1
+            current_agent.direction = EDirection.Left
+        elif action == "right":
+            current_agent.position.x += 1
+            current_agent.direction = EDirection.Right
+
+        # calculate score
+        current_score = new_world.scores[self.my_side] if is_us else new_world.scores[self.other_side]
+        added_score_curr_agent, added_score_opp_curr_agent, current_agent = self.calculate_score(new_world, is_us,
+                                                                                            current_agent)
+        final_score = current_score + added_score_curr_agent
+        if is_us:
+            new_world.scores[self.my_side] = final_score
+            new_world.scores[self.other_side] += added_score_opp_curr_agent
+        else:
+            new_world.scores[self.other_side] = final_score
+            new_world.scores[self.my_side] += added_score_opp_curr_agent
+
+        # update wall breaker
+        if current_agent.wall_breaker_rem_time == 0:
+            current_agent.wall_breaker_cooldown = new_world.constants.wall_breaker_cooldown
+            current_agent.wall_breaker_rem_time = new_world.constants.wall_breaker_duration
+
+        # update world with agent
+        if is_us:
+            new_world.agents[self.my_side] = current_agent
+        else:
+            new_world.agents[self.other_side] = current_agent
+
+        return new_world
 
     def get_actions(self, world, player):
-        # checking if from three directions, there is a wall or not
-        # if there is no wall, we can go to that direction
-        # if there is a wall, we check if we have wall breaker available
-        # if we have, we can break the wall and go to that direction
-        # if we don't have, we can't go to that direction
-
         actions = []
-        my_agent = world.agents[player]
+        cur_pos = player.position
+        for direction in EDirection:
+            if self.is_opposite_direction(direction, player.direction):
+                continue
+            if direction == EDirection.Up:
+                # todo: check if we want to have Deliberate loss
+                # todo: if index out of range error, might be here!
+                ##
+                y_destination = player.position.y - 1
+                x_destination = player.position.x
+                if world.board[y_destination][x_destination] == ECell.AreaWall:
+                    continue
+                else:
+                    if world.board[y_destination][x_destination] == ECell.Empty:
+                        actions.append("up_off")
+                    else:
+                        # hitting our wall
+                        if (self.my_side == "Yellow" and world.board[y_destination][x_destination] == ECell.YellowWall) or \
+                                (self.my_side == "Blue" and world.board[y_destination][x_destination] == ECell.BlueWall):
+                            actions.append("up_on_our")
+                        # hitting enemy wall
+                        else:
+                            actions.append("up_on_enemy")
+            elif direction == EDirection.Down:
+                y_destination = player.position.y + 1
+                x_destination = player.position.x
+                if world.board[y_destination][x_destination] == ECell.AreaWall:
+                    continue
+                else:
+                    if world.board[y_destination][x_destination] == ECell.Empty:
+                        actions.append("down_off")
+                    else:
+                        # hitting our wall
+                        if (self.my_side == "Yellow" and world.board[y_destination][x_destination] == ECell.YellowWall) or \
+                                (self.my_side == "Blue" and world.board[y_destination][x_destination] == ECell.BlueWall):
+                            actions.append("down_on_our")
+                        # hitting enemy wall
+                        else:
+                            actions.append("down_on_enemy")
+            elif direction == EDirection.Left:
+                y_destination = player.position.y
+                x_destination = player.position.x - 1
+                if world.board[y_destination][x_destination] == ECell.AreaWall:
+                    continue
+                else:
+                    if world.board[y_destination][x_destination] == ECell.Empty:
+                        actions.append("left_off")
+                    else:
+                        # hitting our wall
+                        if (self.my_side == "Yellow" and world.board[y_destination][x_destination] == ECell.YellowWall) or \
+                                (self.my_side == "Blue" and world.board[y_destination][x_destination] == ECell.BlueWall):
+                            actions.append("left_on_our")
+                        # hitting enemy wall
+                        else:
+                            actions.append("left_on_enemy")
+            elif direction == EDirection.Right:
+                y_destination = player.position.y
+                x_destination = player.position.x + 1
+                if world.board[y_destination][x_destination] == ECell.AreaWall:
+                    continue
+                else:
+                    if world.board[y_destination][x_destination] == ECell.Empty:
+                        actions.append("right_off")
+                    else:
+                        # hitting our wall
+                        ####
+                        if (self.my_side == "Yellow" and world.board[y_destination][x_destination] == ECell.YellowWall) or \
+                                (self.my_side == "Blue" and world.board[y_destination][x_destination] == ECell.BlueWall):
+                            actions.append("right_on_our")
+                        # hitting enemy wall
+                        else:
+                            actions.append("right_on_enemy")
+        return actions
+
+    def is_opposite_direction(self, direction1, direction2):
+        if direction1 == EDirection.Up and direction2 == EDirection.Down:
+            return True
+        if direction1 == EDirection.Down and direction2 == EDirection.Up:
+            return True
+        if direction1 == EDirection.Left and direction2 == EDirection.Right:
+            return True
+        if direction1 == EDirection.Right and direction2 == EDirection.Left:
+            return True
+        return False
+
+    def initialize(self):
+        self.i = 1
+        pass
+
 
     def decide(self):
+        self.i += 1
 
-        # bounded depth MinMax Tree Search
-        # calculating heuristics by using the our MagicalBrain
-        # choosing the best move
+        # depth = 1
+        # self.min_max_tree(depth, self.world)
 
-        self.send_command()
-        if self.world.agents[self.my_side].wall_breaker_cooldown == 0:
-            self.send_command(ActivateWallBreaker())
+        # test
+        best_move = None
+        best_score = float('-inf')
+        actions = self.get_actions(self.world, player=self.world.agents[self.my_side])
+        if self.i >= 30:
+            print("actions in i : " + str(self.i) + " are: " + actions.__str__())
+        for action in actions:
+            next_direction = action.split("_")[0]
+            # activate_state = action.split("_")[1:]
+            new_world = copy.deepcopy(self.world)
+            new_world = self.game_result(new_world, next_direction, is_us=True)
+            # new_score = self.min_val(depth, new_world)
+            new_score = new_world.scores[self.my_side]
+            if new_score > best_score:
+                best_score = new_score
+                best_move = action
+                self.world = new_world
 
+        split_move = best_move.split("_")[0]
+        if split_move == "up":
+            self.send_command(ChangeDirection(EDirection.Up))
+        elif split_move == "down":
+            self.send_command(ChangeDirection(EDirection.Down))
+        elif split_move == "left":
+            self.send_command(ChangeDirection(EDirection.Left))
+        elif split_move == "right":
+            self.send_command(ChangeDirection(EDirection.Right))
+
+        print("our score : " + str(self.world.scores[self.my_side]))
+        print("enemy score : " + str(self.world.scores[self.other_side]))
     # min max tree func
     def min_max_tree(self, depth, world):
 
-        # as while we call this function, always it is my turn to play
-        # so we should call max_val_func
-
-        move = None
-        v = float('-inf')
+        best_move = None
+        best_score = float('-inf')
         for action in self.get_actions(world, player=self.my_side):
-            # deep copy from world
+            next_direction = action.split("_")[0]
+            activate_state = action.split("_")[1:]
             new_world = copy.deepcopy(world)
-            new_world = game_result(new_world, action, player=self.my_side)
-            v2 = self.min_val(depth, new_world)
-            if v2 > v:
-                v = v2
+            new_world = self.game_result(new_world, next_direction, is_us=True)
+            new_score = self.min_val(depth, new_world)
+            if new_score > best_score:
+                best_score = new_score
+                best_move = action
+                world = new_world
+        # change agent direction to best move
+        # todo: activate wall breaker if needed
 
-        return v, move
+        return best_score, best_move
 
     def min_val(self, depth, world):
         # if depth == 0:
@@ -98,18 +291,19 @@ class AI(RealtimeAI):
         # modified world
         # return v
 
+        # min is max of other player
         if depth == 0:
             return self.heuristic(world)
-        v = float('inf')
+        v = float('-inf')
         depth -= 1
         for action in self.get_actions(world, player=self.my_side):
             # deep copy from world
             new_world = copy.deepcopy(world)
-            new_world = game_result(new_world, action, player=self.my_side)
-            # v2, a2 = self.max_val(depth, new_world)
+            new_world = self.game_result(new_world, action, player=self.other_side)
             v2 = self.max_val(depth, new_world)
-            if v2 < v:
+            if v2 > v:
                 v = v2
+        return v
 
     def max_val(self, depth, world):
         if depth == 0:
@@ -119,8 +313,7 @@ class AI(RealtimeAI):
         for action in self.get_actions(world, player=self.my_side):
             # deep copy from world
             new_world = copy.deepcopy(world)
-            new_world = game_result(new_world, action, player=self.my_side)
-            # v2, a2 = self.min_val(depth, new_world)
+            new_world = self.game_result(new_world, action, player=self.my_side)
             v2 = self.min_val(depth, new_world)
             if v2 > v:
                 v = v2
